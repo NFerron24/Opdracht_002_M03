5c2e7ba1c7af8b1ed24cb4c5b1390cc2dcbfa923
NFerron24 <67276148+NFerron24@users.noreply.github.com>
2020-07-20 10:15:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten");
+// Dit is een grote som (de uitkomst van de comment zal niet te zien zijn in de console omdat het niet gelezen wordt als code vanwege de //)
+/*let som_1 = 1230941 * 1823794;
+console.log(som_1);
+let som_2 = 637263 / 54;
+console.log(som_2);*/
+//(nu is zijn de uitkomsten niet meer zichtbaar omdat de blok comment eromheen is 'gewrapt')
